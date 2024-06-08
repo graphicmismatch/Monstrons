ea39a25fe2e21a4cd515d316ad2991395d899c8f
polyesterswing <kurianjojo2004@gmail.com>
2024-06-08 23:41:18 +0530

    Added MonstronData

---
+++ Assets/Scripts/MonstronData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public struct Move {
+    int damage;
+    float chance;
+}
+
+public class MonstronData
+{
+    public int id;
+    public int level;
+    public int exp;
+    public Move[] moves;
+    
+}
